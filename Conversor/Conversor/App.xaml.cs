c9a0c02124afddb4064d0529bccebe9449e41d37
Rafael Moreno <rafael.moreno@symetry.mx>
2022-10-12 08:01:59 -0500

    Version 0.0.0.1 :First commit

---
+++ Conversor/Conversor/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Conversor.Services;
+using Conversor.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Conversor/Conversor/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Conversor.Models;
+using Conversor.ViewModels;
+using Conversor.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
